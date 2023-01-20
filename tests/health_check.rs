929dc7e05d8dd48c1ef293a550e6cc734be8b8ff
Timothy Brackett <brackett.tc@gmail.com>
2023-01-20 18:15:57 -0500

    Run tests using randomly assigned ports

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,24 +2,27 @@ use axum::Server;
 use std::net::TcpListener;
 use zero2prod_axum::run;
 
-fn spawn_app() {
+fn spawn_app() -> String {
     let app = run().expect("Failed to create router.");
-    let listener = TcpListener::bind( "127.0.0.1:8000").expect("");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind port.");
+    let port = listener.local_addr().unwrap().port();
     tokio::spawn(async move {
-        Server::from_tcp(listener).unwrap()
+        Server::from_tcp(listener)
+            .unwrap()
             .serve(app.into_make_service())
             .await
             .unwrap()
     });
+    format!("http://127.0.0.1:{}", port)
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let host = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", host))
         .send()
         .await
         .expect("Failed to execute request.");
