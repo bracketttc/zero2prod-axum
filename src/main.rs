e6de927137b5e43b5bef57aba7e4d4dd0c7dee8d
Timothy Brackett <brackett.tc@gmail.com>
2023-01-20 13:19:51 -0500

    Implement health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{extract::Path, response::IntoResponse, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
 use std::net::SocketAddr;
 
 async fn greet(name: Option<Path<String>>) -> impl IntoResponse {
@@ -8,11 +8,16 @@ async fn greet(name: Option<Path<String>>) -> impl IntoResponse {
     }
 }
 
+async fn health_check() -> impl IntoResponse {
+    StatusCode::OK
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(|| greet(None)))
-        .route("/:name", get(greet));
+        .route("/:name", get(greet))
+        .route("/health_check", get(health_check));
     axum_server::bind(SocketAddr::from(([127, 0, 0, 1], 8000)))
         .serve(app.into_make_service())
         .await
