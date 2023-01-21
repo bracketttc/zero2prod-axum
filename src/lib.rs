ced547fbb0a7e8fd070ee88aa8051ae76ce3934a
Timothy Brackett <brackett.tc@gmail.com>
2023-01-21 10:11:37 -0500

    Add subscriptions endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,17 @@
-use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{
+    extract::{Form, Path},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Router,
+};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 async fn greet(name: Option<Path<String>>) -> impl IntoResponse {
     match name {
@@ -11,10 +24,15 @@ async fn health_check() -> impl IntoResponse {
     StatusCode::OK
 }
 
+async fn subscribe(form: Form<FormData>) -> impl IntoResponse {
+    StatusCode::OK
+}
+
 pub fn run() -> Result<Router, std::io::Error> {
     let app = Router::new()
         .route("/", get(|| greet(None)))
         .route("/:name", get(greet))
-        .route("/health_check", get(health_check));
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe));
     Ok(app)
 }
