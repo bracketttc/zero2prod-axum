f778a38f5c4242b23045081bd1818e622daee7a9
Timothy Brackett <brackett.tc@gmail.com>
2023-01-28 20:43:28 -0500

    Apply formatting rules

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -13,10 +13,10 @@ static TRACING: Lazy<()> = Lazy::new(|| {
     let subscriber_name = "test".to_string();
 
     if std::env::var("TEST_LOG").is_ok() {
-        let subscriber = get_subscriber(subscriber_name, default_filter_level,std::io::stdout);
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
         init_subscriber(subscriber);
-    }else {
-        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink );
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
         init_subscriber(subscriber);
     }
 });
