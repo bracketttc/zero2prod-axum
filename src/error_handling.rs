822067182dafbe36e11ede68834cac634e96ce42
Timothy Brackett <brackett.tc@gmail.com>
2023-02-09 19:07:03 -0500

    Improve error logging for subscription handler
    This is about as close as I can get to parity with Luca's implementation
    with actix_web. Axum doesn't want to acknowledge that things fail I
    guess, so I'm using a call to the `tracing::error` macro in the
    conversion of my handler error type to an `axum::response::Response` to
    work around that. The http request span doesn't reiterate the error
    message, though.

---
+++ src/error_handling.rs
@@ -0,0 +1,13 @@
+/// Print a stacktrace-esque list of errors that caused the current error.
+pub fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{e}\n")?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{cause}")?;
+        current = cause.source();
+    }
+    Ok(())
+}
